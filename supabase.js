bc785d900269f8dc8d495aa56a45f76cbf0bdf46
Aaoza Travels <adminaaozatravel@gmail.com>
2026-04-29 13:21:20 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,14 @@
 const SUPABASE_URL = "https://yejnsszmvlnmkfqhyfdc.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inllam5zc3ptdmxubWtmcWh5ZmRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMzI2MTYsImV4cCI6MjA4OTkwODYxNn0.0hOShgfmb6NIITSzulKvIePerRa-ofLZ55TU-v3k6s8";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storage: localStorage
+  }
+});
 window.supabaseClient = supabaseClient;
 
 
